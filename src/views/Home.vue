6dbd99cfaf24f1407509cca3b72fbece946fd7bd
aqib04 <aqib@oneorigin.us>
2022-03-03 18:01:40 +0530

    check this outgit status!

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <p>Heyya welcome to my Blogger, Check out our post's.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
